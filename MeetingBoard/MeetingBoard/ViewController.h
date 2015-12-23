de02abea0d101b13787ccdfc48c7090335ff847a
寺内 信夫 <tenobu@gmail.com>
2015-12-23 17:16:40 +0900

    first commit

---
+++ MeetingBoard/MeetingBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetingBoard
+//
+//  Created by 寺内 信夫 on 2015/12/23.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MeetingBoard/MeetingBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeetingBoard
+//
+//  Created by 寺内 信夫 on 2015/12/23.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
